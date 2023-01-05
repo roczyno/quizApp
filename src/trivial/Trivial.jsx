60b66c70e82ec35a0d5385a976b14a909ba0236f
adiaba jacob <adiabajacob9@gmail.com>
2023-01-05 20:13:12 +0000

    done with the functionalities

--- src/trivial/Trivial.jsx
+++ src/trivial/Trivial.jsx
@@ -1,16 +1,54 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import "./trivial.scss";
 
-const Trivial = () => {
+const Trivial = ({ data, setStop, questionNumber, setQuestionNumber }) => {
+  const [question, setQuestion] = useState(null);
+
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+  const [className, setClassName] = useState("answer");
+
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
+  useEffect(() => {
+    setQuestion(data[questionNumber - 1]);
+  }, [data, questionNumber]);
+
+  const handleClick = (a) => {
+    setSelectedAnswer(a);
+    setClassName("answer active");
+
+    delay(3000, () =>
+      setClassName(a.correct ? "answer correct" : "answer wrong")
+    );
+
+    delay(6000, () => {
+      if (a.correct) {
+        setQuestionNumber((prev) => prev + 1);
+        setSelectedAnswer(null);
+      } else {
+        setStop(true);
+      }
+    });
+  };
+
   return (
     <div className="trivial">
-      <div className="question">Which is the best school in Ghana</div>
-      <div className="questions">
-        <div className="question wrong">University of Ghana</div>
-        <div className="question correct">University of Education</div>
-        <div className="question">University of CApe Coast</div>
-        <div className="question">
-          Kwamw Nkrumah University of Science and Technology
-        </div>
+      <div className="question">{question?.question}</div>
+      <div className="answers">
+        {question?.answers.map((a) => (
+          <div
+            className={selectedAnswer === a ? className : "answer"}
+            onClick={() => handleClick(a)}
+          >
+            {a.text}
+          </div>
+        ))}
       </div>
     </div>
   );
