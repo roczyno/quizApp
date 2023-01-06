6f2784112e8facb7c31d71a308fc757c7ae0e8e8
adiaba jacob <adiabajacob9@gmail.com>
2023-01-06 20:00:30 +0000

    {

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      {/* <Home /> */}
-      <Login />
-      {/* <Register /> */}
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
